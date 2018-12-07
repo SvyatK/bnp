6c41591eaed1558755f5939015bdf2b20f329b85
Andrii Pechera <andrew.pechera@gmail.com>
2018-12-07 21:36:07 +0200

    add constats with dimensions

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,8 @@
 import * as PIXI from 'pixi.js';
 import basePic from '../img/base.png';
+import { GAME_X_PX, GAME_Y_PX } from './constants';
 
-const main = new PIXI.Application(800, 600, {backgroundColor : 0x000000});
+const main = new PIXI.Application(GAME_X_PX, GAME_Y_PX, { backgroundColor : 0x000000 });
 const base = PIXI.Sprite.fromImage(basePic);
 
 main.stage.addChild(base);
