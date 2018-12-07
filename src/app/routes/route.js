3239a80d9b74782e896fe652e82324a15bdf7591
dimakiev <secondqueen@ukr.net>
2018-12-07 21:54:17 +0200

    code optimization

--- src/app/routes/route.js
+++ src/app/routes/route.js
@@ -3,8 +3,8 @@ class Route {
     static PAGES = ['mode', 'stage', 'scores', 'upgrade', 'won', 'over', 'scenario/1', 'scenario/2', 'scenario/3']; 
 
     static getPage() {
-        const path = window.location.pathname;
-        this.page = (!path || path === '/') ? 'mode' : window.location.pathname.substr(1);
+        const path = window.location.hash;
+        this.page = (!path || path === '/') ? 'mode' : path.substr(2);
         if (!Route.PAGES.includes(this.page)) {
             this.page = 'mode';
         }
