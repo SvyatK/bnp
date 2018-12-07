a0c3c5c5cf56c6fbab037679c90bf323bc226e65
dimakiev <secondqueen@ukr.net>
2018-12-07 20:33:19 +0200

    code optimization

--- src/app/engine/bootstrap.js
+++ src/app/engine/bootstrap.js
@@ -8,11 +8,20 @@ const road = new Road();
 const bridge = new Bridge();
 const wall = new Wall();
 const water = new Water();
-const tank = new Tank();
-tank.setAmmo(44);
-tank.setArmour(123);
-tank.setSpeed(11);
-console.log(tank);
+
+const tanks = [];
+fetch('/stages/1/units.json')
+  .then(response => response.json())
+  .then(function(units) {
+      for(const unit of units) {
+        const tank = new Tank();
+        tank.setAmmo(unit.ammo);
+        tank.setArmour(unit.armour);
+        tank.setSpeed(unit.speed);
+        tanks.push(tank);
+      }
+});
+console.log(tanks);
 
 console.log('bootstrap');
 
