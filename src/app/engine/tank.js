bdf991b444063d68a703692cfa3ee2b8924e3a0f
dimakiev <secondqueen@ukr.net>
2018-12-08 00:14:01 +0200

    code optimization

--- src/app/engine/tank.js
+++ src/app/engine/tank.js
@@ -2,6 +2,38 @@ import Unit from './unit';
 class Tank extends Unit {
     constructor() {
         super();
+        this.accuracy = 0;
+        this.minDamage = 0;
+        this.maxDamage = 0;
+    }
+
+    setMinDamage(value) {
+        this.minDamage = parseInt(value);
+    }
+
+    setMaxDamage(value) {
+        this.maxDamage = parseInt(value);
+    }
+
+    setDamage(min, max) {
+        this.setMinDamage(min);
+        this.setMaxDamage(max);
+    }
+
+    getMinDamage() {
+        return this.minDamage;
+    }
+
+    getMaxDamage() {
+        return this.maxDamage;
+    }
+
+    setAccuracy(value) {
+        this.accuracy = parseInt(value);
+    }
+
+    getAccuracy() {
+        return this.accuracy;
     }
 
     fire() {
