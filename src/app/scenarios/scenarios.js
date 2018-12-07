1a440c7aa3841d134a148819ab1bacfad92b3685
SvyatK <Svyat_@ukr.net>
2018-12-08 00:43:35 +0200

    implement lever drawing

--- src/app/scenarios/scenarios.js
+++ src/app/scenarios/scenarios.js
@@ -2,10 +2,14 @@ import Road from '../engine/road';
 import Bridge from '../engine/bridge';
 import Wall from '../engine/wall';
 import Water from '../engine/water';
+import Route from '../routes/route';
+import main from '../main';
+import * as PIXI from 'pixi.js';
+import * as Constants from '../constants.js';
 
 function onNavigation(){
-    if (window.location.hash.includes('scenario')) {
-        getLevelData(window.location.hash.split("/")[2]);
+    if (Route.getPage().includes('scenario')) {
+        getLevelData(Route.getPage().split("/")[1]);
     }
 }
 
@@ -16,31 +20,58 @@ function getLevelData(level) {
         .then(response => response.json())
         .then((data) => {
             const result = [];
-            for (const row of data.scheme) {
-                for (const item of row) {
+            for (let i = 0; i < data.scheme.length; i++) {
+                const row = data.scheme[i];
+                for (let j = 0; j < row.length; j++) {
+                    const item = row[j];
                     switch (item) {
                         case '0':{
                             const road = new Road();
+                            road.setCoordinates((j % 13) * Constants.TILE_PX, i * Constants.TILE_PX);
                             result.push(road);
                             break;
                         }
                         case 'w':{
                             const wall = new Wall();
+                            wall.setCoordinates((j % 13) * Constants.TILE_PX, i * Constants.TILE_PX);
                             result.push(wall);
                             break;
                         }
                         case 'r':{
                             const water = new Water();
+                            water.setCoordinates((j % 13) * Constants.TILE_PX, i * Constants.TILE_PX);
                             result.push(water);
                             break;
                         }
                         case 'b':{
                             const bridge = new Bridge();
+                            bridge.setCoordinates((j % 13) * Constants.TILE_PX, i * Constants.TILE_PX);
                             result.push(bridge);
                             break;
                         }
                     }
                 }
+            }
+
+            main.stage.removeChildren();
+
+            var background = new PIXI.Graphics();
+
+            background.beginFill(0xcccccc, 1);
+            background.drawRect(0, 0, Constants.GAME_X_PX, Constants.GAME_Y_PX);
+            background.endFill();
+
+            main.stage.addChild(background);
+
+            for (const resultItem of result) {
+                const texture = PIXI.Texture.fromImage(resultItem.getImgSource());
+
+                const graphicItem = new PIXI.Sprite(texture);
+                graphicItem.x = Constants.TILE_PX + resultItem.getPosX();
+                graphicItem.y = Constants.TILE_PX/2 + resultItem.getPosY();
+                graphicItem.height = Constants.TILE_PX;
+                graphicItem.width = Constants.TILE_PX;
+                main.stage.addChild(graphicItem);
             }
             return result;
         });
