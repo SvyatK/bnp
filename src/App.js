accb79a2a9616d2028b016935ff6dc475ef1e6ec
dimakiev <secondqueen@ukr.net>
2018-12-07 18:19:29 +0200

    add structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './app/menu/menu.js';
+import './app/engine/engine.js';
+import './app/scenarios/scenarios.js';
+import './app/controller/keyboard.js';
+import './app/controller/gamepad.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <canvas id="app" />
     );
   }
 }
