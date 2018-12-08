f3cdf7aaad9212c2413dcfda24c6b3cb448b41f4
high-degree <dmitrii.grechukha@gmail.com>
2018-12-08 02:44:58 +0200

    style: deleted odd engine file.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import './app/menu/menu.js';
-import './app/engine/engine.js';
 import './app/scenarios/scenarios.js';
 import './app/controller/keyboard.js';
 import './app/controller/gamepad.js';
