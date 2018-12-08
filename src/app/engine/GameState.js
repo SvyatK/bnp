33ffc05f64c7e56c884515f751949c1d2af91988
high-degree <dmitrii.grechukha@gmail.com>
2018-12-08 07:51:29 +0200

    WIP

--- src/app/engine/GameState.js
+++ src/app/engine/GameState.js
@@ -5,22 +5,26 @@ import * as pull from 'lodash.pull';
 
 export default class GameState {
     next() {
-        units.forEach((unit) => {
-           unit.randomIntent();
-        });
+        units
+            .filter((unit) => {
+                return !(/player/.test(unit.key));
+            })
+            .forEach((unit) => {
+               unit.randomIntent();
+            });
 
         // ===  resolve conflicts
 
         // check if collision exist
-        const moveEvents = EventBus._queue.filter(intent => /MOVE/.test(intent.eventName));
-        EventBus._queue.forEach((intent) => {
-            if (/MOVE/.test(intent.eventName)) {
+        const moveEvents = EventBus._queue.filter(ent => /MOVE/.test(ent.eventName));
+        EventBus._queue.forEach((ent) => {
+            if (/MOVE/.test(ent.eventName)) {
                 moveEvents
                     .filter((innt) => {
-                        return innt.object !== intent.object;
+                        return innt.object !== ent.object;
                     })
                     .filter((innt) => {
-                        return isIntersects(innt.object, intent.object);
+                        return isersects(innt.object, ent.object);
                     })
                     .forEach((innt) => {
                         // resolve collisions
@@ -29,35 +33,38 @@ export default class GameState {
             }
         });
 
-        // fulfill intents
-        EventBus._queue.forEach((intent) => {
-            intent.object.act(intent.eventName);
+        // fulfill ents
+        EventBus._queue.forEach((ent) => {
+            ent.object.act(ent.eventName);
         });
         EventBus._queue = [];
     }
 }
 
-function isIntersects ( a, b ) {
-    const ax = a.posX;
-    const ax1 = a.posX+TILE_PX;
-    const ay = a.posY;
-    const ay1 = a.posY+TILE_PX;
-    const bx = b.posX;
-    const bx1 = b.posX+TILE_PX;
-    const by = b.posY;
-    const by1 = b.posY+TILE_PX;
+/*
+    x1, y1 - левая нижняя точка первого прямоугольника
+    x2, y2 - правая верхняя точка первого прямоугольника
+    x3, y3 - левая нижняя точка второго прямоугольника
+    x4, y4 - правая верхняя точка второго прямоугольника
+*/
 
-    return ( ay < by1 || ay1 > by || ax1 < bx || ax > bx1 );
+function isersects(o1, o2) {
+    const x1 = o1.posX;
+    const y1 = o1.posY+TILE_PX;
+    const x2 = o1.posX+TILE_PX;
+    const y2 = o1.posY;
+    const x3 = o2.posX;
+    const y3 = o2.posY+TILE_PX;
+    const x4 = o2.posX+TILE_PX;
+    const y4 = o2.posY;
+
+    const left = Math.max(x1, x3);
+    const top = Math.min(y2, y4);
+    const right = Math.min(x2, x4);
+    const bottom = Math.max(y1, y3);
+
+    const width = right - left;
+    const height = top - bottom;
+
+    return !(width < 0 || height < 0);
 }
-
-// (a.x,a.y)--------------|
-//    |                   |
-//    |                   |
-//    |                   |
-//    |---------------(a.x1,a.y1)
-//
-//        (b.x,b.y)---------------------|
-//           |                          |
-//           |                          |
-//           |                          |
-//           |---------------------(b.x1,b.y1)
