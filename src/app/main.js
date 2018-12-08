eaf22d02a4d8c33cddb3c236c2b7a044f709cf1b
Andrii Pechera <andrew.pechera@gmail.com>
2018-12-08 03:53:34 +0200

    render main menu '/mode'

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,10 @@
 import * as PIXI from 'pixi.js';
-import basePic from '../img/base.png';
+// import basePic from '../img/base.png';
 import { GAME_X_PX, GAME_Y_PX } from './constants';
 
 const main = new PIXI.Application(GAME_X_PX, GAME_Y_PX, { backgroundColor : 0x000000 });
-const base = PIXI.Sprite.fromImage(basePic);
+// const base = PIXI.Sprite.fromImage(basePic);
 
-main.stage.addChild(base);
+// main.stage.addChild(base);
 
 export default main;
--- src/app/menu/menu.js
+++ src/app/menu/menu.js
@@ -1,10 +1,15 @@
 import Route from '../routes/route';
 import modeMenuItems from './modeMenuItems';
+import main from '../main';
+import * as PIXI from 'pixi.js';
+import { TILE_PX } from '../constants';
 
 class Menu {
-  constructor() {
+  constructor(title, { stage }) {
     this._selected_item = 0;
     this._items = [];
+    this._stage_ctx = stage;
+    this._title = title;
   }
 
   setItems(items) {
@@ -28,17 +33,37 @@ class Menu {
 
   executeCurrentItem() {
     this.getCurrentItem().execute();
-  }  
+  }
+
+  renderMenu() {
+    this._stage_ctx.removeChildren();
+    const stage = this._stage_ctx;
+    const style = new PIXI.TextStyle({
+      fill: 'white',
+      fontFamily: 'Arial',
+    });
+    const title = new PIXI.Text(this._title, style);
+    title.x = TILE_PX * 3;
+    title.y = TILE_PX * 4;
+    stage.addChild(title);
+    this._items
+      .forEach((item, i) => {
+        const textLine = new PIXI.Text(item.getTitle(), style);
+        textLine.x = TILE_PX * 4;
+        textLine.y = (TILE_PX * 6) + i * TILE_PX;
+        stage.addChild(textLine);
+      });
+  }
 };
 
 const onNavigation = () => {
-  const currentPage = Route.getPage();
-  switch (currentPage) {
+  switch (Route.getPage()) {
     case Route.PAGES[0]: {
       console.log(Route.PAGES[0]);
-      const menu = new Menu();
+      const menu = new Menu('Select Mode:', main);
       menu.setItems(modeMenuItems);
-      menu.getCurrentItem().execute();
+      menu.renderMenu();
+      // menu.getCurrentItem().execute();
       break;
     }
     case Route.PAGES[1]: {
