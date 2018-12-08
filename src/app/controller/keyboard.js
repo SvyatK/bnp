8072963769a765afe633f6d93e0404a57432ead8
high-degree <dmitrii.grechukha@gmail.com>
2018-12-08 09:57:51 +0200

    Added arrow key handlers.

--- src/app/controller/keyboard.js
+++ src/app/controller/keyboard.js
@@ -1,1 +1,50 @@
-console.log('keyboard');
+import {units} from "../scenarios/scenarios";
+import EventBus, {Events} from "../engine/EventBus";
+
+window.addEventListener('keydown', (event) => {
+
+    if (event.code === 'ArrowUp'
+        || event.code === 'ArrowDown'
+        || event.code === 'ArrowLeft'
+        || event.code === 'ArrowRight') {
+        event.preventDefault();
+    }
+
+    switch (event.code) {
+        case 'ArrowUp': {
+            units.forEach((unit) => {
+                if (unit.key.includes('player1')) {
+                    EventBus.playerReveal(Events.TANK_MOVE_UP, unit);
+                }
+            });
+            break;
+        }
+        case 'ArrowDown': {
+            units.forEach((unit) => {
+                if (unit.key.includes('player1')) {
+                    EventBus.playerReveal(Events.TANK_MOVE_DOWN, unit);
+                }
+            });
+
+            break;
+        }
+        case 'ArrowLeft': {
+            units.forEach((unit) => {
+                if (unit.key.includes('player1')) {
+                    EventBus.playerReveal(Events.TANK_MOVE_LEFT, unit);
+                }
+            });
+
+            break;
+        }
+        case 'ArrowRight': {
+            units.forEach((unit) => {
+                if (unit.key.includes('player1')) {
+                    EventBus.playerReveal(Events.TANK_MOVE_RIGHT, unit);
+                }
+            });
+
+            break;
+        }
+    }
+});
