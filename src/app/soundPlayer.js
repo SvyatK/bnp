a94ab17fd228587f7a42e648c3ad8c976aa9b75d
high-degree <dmitrii.grechukha@gmail.com>
2018-12-08 11:45:54 +0200

    hotfix

--- src/app/soundPlayer.js
+++ src/app/soundPlayer.js
@@ -1,23 +1,21 @@
-const soundsNamesList = [
-    'stage_start',
-    'game_over',
-    'bullet_shot',
-    'bullet_hit_1',
-    'bullet_hit_2',
-    'explosion_1',
-    'explosion_2',
-    'pause',
-    'powerup_appear',
-    'powerup_pick',
-    'statistics_1',
-];
-
-const soundMap = new Map();
-soundsNamesList.map(sound => ([sound, new Audio(`sound/${sound}.ogg`)]))
-    .forEach(([k, v]) => {
-        soundMap.set(k,v);
-    });
-
-export default function playSound(sound) {
-    soundMap.get(sound).play();
+const sounds = {
+    stage_start: null,
+    game_over: null,
+    bullet_shot: null,
+    bullet_hit_1: null,
+    bullet_hit_2: null,
+    explosion_1: null,
+    explosion_2: null,
+    pause: null,
+    powerup_appear: null,
+    powerup_pick: null,
+    statistics_1: null,
 };
+
+for (let soundName in sounds) {
+    sounds[soundName] = new Audio(`sounds/${soundName}.ogg`);
+}
+
+export default function playSound(soundName) {
+    sounds[soundName].play();
+}
